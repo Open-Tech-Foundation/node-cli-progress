dbf1e4cb5a581e1f3538486dfda85bef5e7535ea
Thanga Ganapathy <ganapathy888@gmail.com>
2021-08-10 10:16:43 +0530

    Add clean plugin to rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,14 @@
+import typescript from '@rollup/plugin-typescript';
+import { clean } from '@open-tech-world/rollup-plugin-clean';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      dir: './lib',
+      format: 'esm',
+    },
+  ],
+  plugins: [clean('lib/*'), typescript({ tsconfig: './tsconfig.json' })],
+  external: ['@open-tech-world/es-cli-styles'],
+};
