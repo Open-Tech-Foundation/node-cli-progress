e916a7c09db02f86cb648fd5e2f739d154388e3e
Thanga Ganapathy <ganapathy888@gmail.com>
2021-08-10 11:40:43 +0530

    Update status only if it is in running state

--- src/cliProgress.ts
+++ src/cliProgress.ts
@@ -1,21 +1,29 @@
 import { style } from '@open-tech-world/es-cli-styles';
 
 class cliProgress {
-  stream: NodeJS.WriteStream;
-  isRunning: boolean;
-  text: string;
+  private stream: NodeJS.WriteStream;
+  private isRunning: boolean;
+  private text: string;
 
   constructor(stream: NodeJS.WriteStream) {
     this.stream = stream || process.stderr;
     this.isRunning = false;
-    this.text = 'Loading';
+    this.text = 'Loading ';
+  }
+
+  private setText(text: string): void {
+    if (!this.isRunning) {
+      return;
+    }
+
+    if (text) {
+      this.text = text + ' ';
+    }
   }
 
   start(text: string): void {
-    if (text) {
-      this.text = text;
-    }
     this.isRunning = true;
+    this.setText(text);
     const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
     let currentPosition = 0;
     const intervalInstance = setInterval(() => {
@@ -37,46 +45,56 @@ class cliProgress {
   }
 
   update(text: string): void {
-    if (this.isRunning) {
-      this.text = text;
-    }
+    this.setText(text);
   }
 
   done(text: string): void {
-    if (text) {
-      this.text = text;
+    if (!this.isRunning) {
+      return;
     }
+
+    this.setText(text);
     this.isRunning = false;
+
     if (this.stream.clearLine(0) && this.stream.cursorTo(0)) {
       this.stream.write('✔️  ' + this.text + '\n');
     }
   }
 
   fail(text: string): void {
-    if (text) {
-      this.text = text;
+    if (!this.isRunning) {
+      return;
     }
+
+    this.setText(text);
     this.isRunning = false;
+
     if (this.stream.clearLine(0) && this.stream.cursorTo(0)) {
       this.stream.write('❌ ' + this.text + '\n');
     }
   }
 
   warn(text: string): void {
-    if (text) {
-      this.text = text;
+    if (!this.isRunning) {
+      return;
     }
+
+    this.setText(text);
     this.isRunning = false;
+
     if (this.stream.clearLine(0) && this.stream.cursorTo(0)) {
       this.stream.write('⚠️  ' + this.text + '\n');
     }
   }
 
   info(text: string): void {
-    if (text) {
-      this.text = text;
+    if (!this.isRunning) {
+      return;
     }
+
+    this.setText(text);
     this.isRunning = false;
+
     if (this.stream.clearLine(0) && this.stream.cursorTo(0)) {
       this.stream.write('ℹ️  ' + this.text + '\n');
     }
