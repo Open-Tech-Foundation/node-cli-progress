26cbf1f550ff99141169b8012648004dd5f129a6
Thanga Ganapathy <ganapathy888@gmail.com>
2021-08-13 09:45:58 +0530

    Fix test by using passthrough stream

--- __tests__/cliProgress.spec.js
+++ __tests__/cliProgress.spec.js
@@ -1,5 +1,15 @@
+import { PassThrough } from 'stream';
+
 import { cliProgress } from '../lib/index.esm.js';
-import { jest } from '@jest/globals';
+
+function getStream() {
+  const stream = new PassThrough();
+  stream.clearLine = () => true;
+  stream.cursorTo = () => true;
+  stream.isTTY = true;
+
+  return stream;
+}
 
 async function sleep(ms) {
   return new Promise((resolve) => {
@@ -9,154 +19,175 @@ async function sleep(ms) {
   });
 }
 
-const stdErrorSpy = jest.spyOn(process.stderr, 'write');
-stdErrorSpy.mockImplementation(() => true);
+async function getOutput(stream) {
+  const output = [];
+
+  return new Promise((resolve) => {
+    stream.on('data', (data) => {
+      output.push(data.toString());
+    });
+
+    stream.on('end', () => {
+      resolve(output);
+    });
+  });
+}
+
+async function run(cb) {
+  const stream = getStream();
+  const outputPromise = getOutput(stream);
+  const progress = new cliProgress(stream);
+  await cb(progress);
+  stream.end();
+
+  return await outputPromise;
+}
 
 describe('cliProgress', () => {
   test('default loading', async () => {
-    const progress = new cliProgress();
-    progress.start();
-    await sleep(100);
-    progress.done();
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start();
+      await sleep(100);
+      progress.done();
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Loading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;46;204;64m✔️\x1B[39;2;m\x1B[39;2;m  Loading \n'
+      '\x1B[1;38;2m\x1B[38;2;46;204;64m\u{2714}\u{FE0F}\x1B[39;2;m\x1B[39;2;m  Loading \n'
     );
   });
 
   it('changes default loading to Downloading', async () => {
-    const progress = new cliProgress();
-    progress.start('Downloading');
-    await sleep(100);
-    progress.done();
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start('Downloading');
+      await sleep(100);
+      progress.done();
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Downloading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;46;204;64m✔️\x1B[39;2;m\x1B[39;2;m  Downloading \n'
+      '\x1B[1;38;2m\x1B[38;2;46;204;64m\u{2714}\u{FE0F}\x1B[39;2;m\x1B[39;2;m  Downloading \n'
     );
   });
 
   test('done with text', async () => {
-    const progress = new cliProgress();
-    progress.start('Downloading');
-    await sleep(100);
-    progress.done('Done!');
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start('Downloading');
+      await sleep(100);
+      progress.done('Done!');
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Downloading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;46;204;64m✔️\x1B[39;2;m\x1B[39;2;m  Done! \n'
+      '\x1B[1;38;2m\x1B[38;2;46;204;64m\u{2714}\u{FE0F}\x1B[39;2;m\x1B[39;2;m  Done! \n'
     );
   });
 
   test('fail', async () => {
-    const progress = new cliProgress();
-    progress.start();
-    await sleep(100);
-    progress.fail();
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start();
+      await sleep(100);
+      progress.fail();
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Loading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;255;65;54m❌\x1B[39;2;m\x1B[39;2;m Loading \n'
+      '\x1B[1;38;2m\x1B[38;2;255;65;54m\u{274C}\u{FE0F}\x1B[39;2;m\x1B[39;2;m Loading \n'
     );
   });
 
   it('fails with text', async () => {
-    const progress = new cliProgress();
-    progress.start();
-    await sleep(100);
-    progress.fail('Network Error!');
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start();
+      await sleep(100);
+      progress.fail('Network Error!');
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Loading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;255;65;54m❌\x1B[39;2;m\x1B[39;2;m Network Error! \n'
+      '\x1B[1;38;2m\x1B[38;2;255;65;54m\u{274C}\u{FE0F}\x1B[39;2;m\x1B[39;2;m Network Error! \n'
     );
   });
 
   test('warn', async () => {
-    const progress = new cliProgress();
-    progress.start();
-    await sleep(100);
-    progress.warn();
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start();
+      await sleep(100);
+      progress.warn();
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Loading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;255;220;0m⚠️\x1B[39;2;m\x1B[39;2;m  Loading \n'
+      '\x1B[1;38;2m\x1B[38;2;255;220;0m\u{26A0}\u{FE0F}\x1B[39;2;m\x1B[39;2;m  Loading \n'
     );
   });
 
   it('warns with text', async () => {
-    const progress = new cliProgress();
-    progress.start();
-    await sleep(100);
-    progress.warn('Caution!');
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start();
+      await sleep(100);
+      progress.warn('Caution!');
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Loading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;255;220;0m⚠️\x1B[39;2;m\x1B[39;2;m  Caution! \n'
+      '\x1B[1;38;2m\x1B[38;2;255;220;0m\u{26A0}\u{FE0F}\x1B[39;2;m\x1B[39;2;m  Caution! \n'
     );
   });
 
   test('info', async () => {
-    const progress = new cliProgress();
-    progress.start();
-    await sleep(100);
-    progress.info();
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start();
+      await sleep(100);
+      progress.info();
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Loading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;127;219;255mℹ️\x1B[39;2;m\x1B[39;2;m  Loading \n'
+      '\x1B[1;38;2m\x1B[38;2;127;219;255m\u{2139}\u{FE0F}\x1B[39;2;m\x1B[39;2;m  Loading \n'
     );
   });
 
   test('info with text', async () => {
-    const progress = new cliProgress();
-    progress.start();
-    await sleep(100);
-    progress.info('Some usefull info');
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start();
+      await sleep(100);
+      progress.info('Some usefull info');
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Loading '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;127;219;255mℹ️\x1B[39;2;m\x1B[39;2;m  Some usefull info \n'
+      '\x1B[1;38;2m\x1B[38;2;127;219;255m\u{2139}\u{FE0F}\x1B[39;2;m\x1B[39;2;m  Some usefull info \n'
     );
   });
 
   it('Updates default text', async () => {
-    const progress = new cliProgress();
-    progress.start();
-    await sleep(100);
-    progress.update('Searching');
-    await sleep(100);
-    progress.done();
-    expect(stdErrorSpy).toBeCalled();
-    const output = stdErrorSpy.mock.calls.flat();
+    const output = await run(async (progress) => {
+      progress.start();
+      await sleep(100);
+      progress.update('Searching');
+      await sleep(100);
+      progress.done();
+    });
+
     expect(output).toContain(
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠋\x1B[39;2;m\x1B[39;2;m Loading '
     );
@@ -164,7 +195,7 @@ describe('cliProgress', () => {
       '\x1B[1;38;2m\x1B[38;2;46;204;64m⠙\x1B[39;2;m\x1B[39;2;m Searching '
     );
     expect(output).toContain(
-      '\x1B[1;38;2m\x1B[38;2;46;204;64m✔️\x1B[39;2;m\x1B[39;2;m  Searching \n'
+      '\x1B[1;38;2m\x1B[38;2;46;204;64m\u{2714}\u{FE0F}\x1B[39;2;m\x1B[39;2;m  Searching \n'
     );
   });
 });
